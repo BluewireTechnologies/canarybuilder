32cdcd1cfdb439a3bb31b0bd97098b9d2faacc44
Alex Davidson <alex.davidson@bluewire-technologies.com>
2016-01-15 13:43:53 +0000

    Implement association between tickets and branches

---
+++ CanaryCollector.UnitTests/Collectors/TicketAndBranchAssociatorTests.cs
@@ -0,0 +1,63 @@
+﻿using System.Linq;
+using CanaryCollector.Collectors;
+using CanaryCollector.Model;
+using NUnit.Framework;
+
+namespace CanaryCollector.UnitTests.Collectors
+{
+    [TestFixture]
+    public class TicketAndBranchAssociatorTests
+    {
+        private TicketAndBranchAssociator sut = new TicketAndBranchAssociator();
+
+        [Test]
+        public void AssociatesTicketWithReferencingBranch()
+        {
+            var ticket = new IssueTicket { Identifier = "E-23456" };
+            var branchName = "bugfix/package-structure-E-23456";
+            
+            var associations = sut.Apply(new[] { ticket }, new[] { branchName }).ToArray();
+
+            Assert.That(associations, Is.EqualTo(new[] {
+                new TicketLinkedBranch { Branch = new Branch { Name = branchName }, Ticket = ticket }
+            }));
+        }
+
+        [Test]
+        public void ExcludesBranchesWhichTargetARelease()
+        {
+            var ticket = new IssueTicket { Identifier = "E-23456" };
+            var branchName = "bugfix/package-structure-E-23456-15.14";
+            
+            var associations = sut.Apply(new[] { ticket }, new[] { branchName }).ToArray();
+
+            Assert.That(associations, Is.Empty);
+        }
+
+        [Test]
+        public void DoesNotAssociateTicketWithBranchReferencingADifferentTicket()
+        {
+            var ticket = new IssueTicket { Identifier = "E-10000" };
+            var branchName = "bugfix/package-structure-E-23456";
+            
+            var associations = sut.Apply(new[] { ticket }, new[] { branchName }).ToArray();
+
+            Assert.That(associations, Is.Empty);
+        }
+
+        [Test]
+        public void AssociatesTicketWithAllValidReferencingBranches()
+        {
+            var ticket = new IssueTicket { Identifier = "E-23456" };
+            var firstBranch = "bugfix/package-structure-first-E-23456";
+            var secondBranch = "bugfix/package-structure-second-E-23456";
+            
+            var associations = sut.Apply(new[] { ticket }, new[] { firstBranch, secondBranch }).ToArray();
+
+            Assert.That(associations, Is.EquivalentTo(new[] {
+                new TicketLinkedBranch { Branch = new Branch { Name = firstBranch }, Ticket = ticket },
+                new TicketLinkedBranch { Branch = new Branch { Name = secondBranch }, Ticket = ticket }
+            }));
+        }
+    }
+}
---
+++ CanaryCollector/Collectors/TicketAndBranchAssociator.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Bluewire.Conventions;
+using CanaryCollector.Model;
+
+namespace CanaryCollector.Collectors
+{
+    /// <summary>
+    /// Associates tickets with branches based on ticket identifier.
+    /// </summary>
+    /// <remarks>
+    /// A ticket may have multiple relevant branches, destined for different release branches. We need
+    /// to identify those branches specifically intended for inclusion in master.
+    /// 
+    /// Bluewire.Conventions defines the expected branch name format. It is assumed that any branch
+    /// specifically targetted at a release is not a candidate for merging into master.
+    /// </remarks>
+    public class TicketAndBranchAssociator
+    {
+        public IEnumerable<TicketLinkedBranch> Apply(IEnumerable<IssueTicket> tickets, IEnumerable<string> availableBranchNames)
+        {
+            var ticketedBranches = CollectTicketBranchesForMaster(availableBranchNames);
+
+            return tickets.Join(ticketedBranches, 
+                t => t.Identifier,
+                b => b.TicketIdentifier,
+                (t, b) => new TicketLinkedBranch { Branch = b.Branch, Ticket = t });
+        }
+
+        private static IEnumerable<BranchWithTicketIdentifier> CollectTicketBranchesForMaster(IEnumerable<string> availableBranchNames)
+        {
+            foreach (var name in availableBranchNames)
+            {
+                StructuredBranch branch;
+                if (!StructuredBranch.TryParse(name, out branch)) continue;
+                if (branch.TargetRelease != null) continue;
+                if (branch.TicketIdentifier == null) continue;
+
+                yield return new BranchWithTicketIdentifier { Branch = new Branch { Name = name }, TicketIdentifier = branch.TicketIdentifier };
+            }
+        }
+
+        struct BranchWithTicketIdentifier
+        {
+            public Branch Branch { get; set; }
+            public string TicketIdentifier { get; set; }
+        }
+    }
+}
