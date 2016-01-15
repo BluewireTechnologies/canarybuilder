5b8b427012aec9319a35c923460b0d48df22eb49
Alex Davidson <alex.davidson@bluewire-technologies.com>
2016-01-15 13:31:06 +0000

    Define branch name format

---
+++ Bluewire.Conventions.UnitTests/StructuredBranchTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Bluewire.Conventions.UnitTests
+{
+    [TestFixture]
+    public class StructuredBranchTests
+    {
+        public struct Case
+        {
+            public string Raw { get; set; }
+            public StructuredBranch Parsed { get; set; }
+            
+            public override string ToString()
+            {
+                return Raw;
+            }
+        }
+
+        /// <summary>
+        /// Test cases defined on the Wiki.
+        /// 
+        /// wiki:DevelopmentWorkflow/BranchNames
+        /// </summary>
+        public static Case[] Cases = {
+            new Case { Raw = "spike/foo-might-be-a-cool-thing", Parsed = new StructuredBranch { Namespace = "spike", Name = "foo-might-be-a-cool-thing" } },
+            new Case { Raw = "personal/chto/working-on-foo-but-dont-have-a-clear-spec-or-issue-number-yet", Parsed = new StructuredBranch { Namespace = "personal/chto", Name = "working-on-foo-but-dont-have-a-clear-spec-or-issue-number-yet" } },
+            new Case { Raw = "feature/fancy-new-foo-E-99998", Parsed = new StructuredBranch { Namespace = "feature", Name = "fancy-new-foo", TicketIdentifier = "E-99998" } },
+            new Case { Raw = "feature/fancy-new-foo-E-99998-15.14", Parsed = new StructuredBranch { Namespace = "feature", Name = "fancy-new-foo", TicketIdentifier = "E-99998", TargetRelease = "15.14" } },
+            new Case { Raw = "bugfix/foo-was-a-bit-buggy-E-99999", Parsed = new StructuredBranch { Namespace = "bugfix", Name = "foo-was-a-bit-buggy", TicketIdentifier = "E-99999" } },
+            new Case { Raw = "bugfix/foo-was-a-bit-buggy-E-99999-2", Parsed = new StructuredBranch { Namespace = "bugfix", Name = "foo-was-a-bit-buggy", TicketIdentifier = "E-99999", NumericSuffix = "2" } },
+            new Case { Raw = "bugfix/foo-was-a-bit-buggy-E-99999-2-14.15", Parsed = new StructuredBranch { Namespace = "bugfix", Name = "foo-was-a-bit-buggy", TicketIdentifier = "E-99999", NumericSuffix = "2", TargetRelease = "14.15" } },
+            new Case { Raw = "refactor/foo-lets-us-do-some-other-stuff-in-a-much-better-way", Parsed = new StructuredBranch { Namespace = "refactor", Name = "foo-lets-us-do-some-other-stuff-in-a-much-better-way" } },
+            new Case { Raw = "refactor/foo-lets-us-do-some-other-stuff-in-a-much-better-way-20160112-1137", Parsed = new StructuredBranch {  Namespace = "refactor", Name = "foo-lets-us-do-some-other-stuff-in-a-much-better-way", NumericSuffix = "20160112-1137" } }
+        };
+
+        [Test]
+        public void CanParse([ValueSource(nameof(Cases))] Case testCase)
+        {
+            var parsed = StructuredBranch.Parse(testCase.Raw);
+            Assert.That(parsed.Namespace, Is.EqualTo(testCase.Parsed.Namespace));
+            Assert.That(parsed.Name, Is.EqualTo(testCase.Parsed.Name));
+            Assert.That(parsed.TicketIdentifier, Is.EqualTo(testCase.Parsed.TicketIdentifier));
+            Assert.That(parsed.NumericSuffix, Is.EqualTo(testCase.Parsed.NumericSuffix));
+            Assert.That(parsed.TargetRelease, Is.EqualTo(testCase.Parsed.TargetRelease));
+        }
+
+        [Test]
+        public void CanFormat([ValueSource(nameof(Cases))] Case testCase)
+        {
+            Assert.That(testCase.Parsed.ToString(), Is.EqualTo(testCase.Raw));
+        }
+    }
+}
---
+++ Bluewire.Conventions/StructuredBranch.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Bluewire.Conventions
+{
+    /// <remarks>
+    /// Branch names need to follow a certain format in order to be machine-parseable.
+    /// 
+    /// [namespace]/[name]-[ticket]-[numeric]-[release]
+    /// 
+    /// [namespace] may have multiple levels.
+    /// [name] may consist of any number of hyphen-separated terms, though more than four is discouraged.
+    /// [ticket] is of the form '{type}-{number}', eg. E-23456.
+    /// [numeric] is optional, but if specified must either be:
+    ///   * 1+ digits, or
+    ///   * a yyyyMMdd datestamp, or.
+    ///   * a yyyyMMdd-HHmm datestamp.
+    /// [release] is optional, but if specified must be a dot-separated version number (usually xx.yy).
+    /// </remarks>
+    public struct StructuredBranch
+    {
+        // Place name first so it's used by default GetHashCode() implementation.
+        private string name;
+        private string @namespace;
+        private string ticketIdentifier;
+        private string numericSuffix;
+        private string targetRelease;
+
+        public string Namespace
+        {
+            get { return @namespace; }
+            set { @namespace = SquashEmptyToNull(value); }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = SquashEmptyToNull(value); }
+        }
+
+        public string TicketIdentifier
+        {
+            get { return ticketIdentifier; }
+            set { ticketIdentifier = SquashEmptyToNull(value); }
+        }
+
+        public string NumericSuffix
+        {
+            get { return numericSuffix; }
+            set { numericSuffix = SquashEmptyToNull(value); }
+        }
+
+        public string TargetRelease
+        {
+            get { return targetRelease; }
+            set { targetRelease = SquashEmptyToNull(value); }
+        }
+
+        public override string ToString()
+        {
+            var mainNameParts = new[] {
+                Name,
+                TicketIdentifier,
+                NumericSuffix,
+                TargetRelease
+            }.Where(p => !String.IsNullOrWhiteSpace(p));
+
+            var mainName = String.Join("-", mainNameParts);
+
+            if (mainName.Length == 0) return "";
+            if (String.IsNullOrWhiteSpace(Namespace)) return mainName;
+            return $"{Namespace}/{mainName}";
+        }
+
+        public static void ValidateBranchName(string raw)
+        {
+            Exception exception;
+            if (!ValidateBranchName(raw, out exception)) throw exception;
+        }
+
+        public static bool TryParse(string raw, out StructuredBranch structured)
+        {
+            Exception exception;
+            return TryParseInternal(raw, out structured, out exception);
+        }
+
+        public static StructuredBranch Parse(string raw)
+        {
+            StructuredBranch structured;
+            Exception exception;
+            if (!TryParseInternal(raw, out structured, out exception)) throw exception;
+            return structured;
+        }
+
+        private static bool ValidateBranchName(string raw, out Exception exception)
+        {
+            if (String.IsNullOrEmpty(raw))
+            {
+                exception = new ArgumentNullException(nameof(raw));
+                return false;
+            }
+            if (raw.StartsWith("/"))
+            {
+                exception = new ArgumentException($"Branch name cannot start with '/': {raw}", nameof(raw));
+                return false;
+            }
+
+            // Git does not require this, I think, but for sanity's sake we're not going to allow it:
+            if (raw.Any(char.IsWhiteSpace))
+            {
+                exception = new ArgumentException("Branch name must not contain whitespace.", nameof(raw));
+                return false;
+            }
+            exception = null;
+            return true;
+        }
+
+        private static bool TryParseInternal(string raw, out StructuredBranch structured, out Exception exception)
+        {
+            structured = new StructuredBranch();
+            
+            if (!ValidateBranchName(raw, out exception)) return false;
+
+            var m = rxBranchStructure.Match(raw);
+            if (!m.Success)
+            {
+                exception = new ArgumentException($"Unable to parse branch name: {raw}");
+                return false;
+            }
+
+            structured.Namespace = m.Groups["namespace"]?.Value;
+            structured.Name = m.Groups["name"].Value;
+            structured.TicketIdentifier = m.Groups["ticketIdentifier"]?.Value;
+            structured.NumericSuffix = m.Groups["numericSuffix"]?.Value;
+            structured.TargetRelease = m.Groups["targetRelease"]?.Value;
+            return true;
+        }
+
+        // Assumes YouTrack ticket identifier format, eg. E-23456
+        private static readonly Regex rxBranchStructure = new Regex(@"
+^
+((?<namespace>\w+(/\w+)*)/)?
+(?<name>\w[-\w]*?)
+(- (?<ticketIdentifier>[A-Z]{1,3}-\d{1,5}))?
+(- (?<numericSuffix> \d{8}(-\d{4})? | \d+ ))?
+(- (?<targetRelease> \d+(.\d+)*))?
+$
+", RegexOptions.IgnorePatternWhitespace | RegexOptions.IgnoreCase);
+
+        private static string SquashEmptyToNull(string str)
+        {
+            if (String.IsNullOrWhiteSpace(str)) return null;
+            return str;
+        }
+    }
+}
