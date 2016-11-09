0da566cc2a993345c0a76a335f9f11c4173dfc34
Alex Davidson <alex.davidson@bluewire-technologies.com>
2016-11-09 13:56:04 +0000

    Add Difference and SymmetricDifference helper objects

---
+++ Bluewire.Common.GitWrapper/Model/Difference.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Bluewire.Common.GitWrapper.Model
+{
+    /// <summary>
+    /// Represents the difference between 'include' and 'exclude', ie:
+    /// * the commits in 'include' which do not exist in 'exclude',
+    /// * exclude..include,
+    /// * ^exclude include
+    /// </summary>
+    public class Difference
+    {
+        private readonly Ref exclude;
+        private readonly Ref include;
+
+        public Difference(Ref exclude, Ref include)
+        {
+            if (exclude == null) throw new ArgumentNullException(nameof(exclude));
+            if (include == null) throw new ArgumentNullException(nameof(include));
+            this.exclude = exclude;
+            this.include = include;
+        }
+
+        public override string ToString()
+        {
+            return $"{exclude}..{include}";
+        }
+
+        public static implicit operator string(Difference difference)
+        {
+            return difference?.ToString();
+        }
+    }
+}
