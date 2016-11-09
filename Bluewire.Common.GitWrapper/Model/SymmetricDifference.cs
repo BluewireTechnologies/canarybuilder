9b0e37eec85f9c957fd4ec67a9facd060b2a054b
Alex Davidson <alex.davidson@bluewire-technologies.com>
2016-11-09 13:56:04 +0000

    Add Difference and SymmetricDifference helper objects

---
+++ Bluewire.Common.GitWrapper/Model/SymmetricDifference.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Bluewire.Common.GitWrapper.Model
+{
+    /// <summary>
+    /// Represents the symmetric difference between 'left' and 'right', ie:
+    /// * the commits in 'left' and 'right' which do not exist in both,
+    /// * left...right,
+    /// * left right --not $(git-merge-base --all left right)
+    /// </summary>
+    public class SymmetricDifference
+    {
+        private readonly Ref left;
+        private readonly Ref right;
+
+        public SymmetricDifference(Ref left, Ref right)
+        {
+            if (left == null) throw new ArgumentNullException(nameof(left));
+            if (right == null) throw new ArgumentNullException(nameof(right));
+            this.left = left;
+            this.right = right;
+        }
+
+        public override string ToString()
+        {
+            return $"{left}...{right}";
+        }
+
+        public static implicit operator string(SymmetricDifference difference)
+        {
+            return difference?.ToString();
+        }
+    }
+}
