d56fcd25de83b6522deab14f6ee3773d31befd49
Chris Tomlinson <chris.tomlinson@bluewire-technologies.com>
2017-03-23 15:02:42 +0000

    SemanticVersion support in Bluewire.Conventions

---
+++ Bluewire.Conventions.UnitTests/SemanticVersionTests.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+
+namespace Bluewire.Conventions.UnitTests
+{
+    [TestFixture]
+    public class SemanticVersionTests
+    {
+        public struct Case
+        {
+            public string Raw { get; set; }
+            public SemanticVersion Parsed { get; set; }
+
+            public override string ToString()
+            {
+                return Raw;
+            }
+        }
+        
+        public static Case[] Cases = {
+            new Case { Raw = "17.05.123-beta", Parsed = new SemanticVersion("17","05",123,"beta") },
+            new Case { Raw = "17.05.123-rc", Parsed = new SemanticVersion("17","05",123,"rc") },
+            new Case { Raw = "17.05.123-canary", Parsed = new SemanticVersion("17","05",123,"canary") },
+            new Case { Raw = "17.05.123-release", Parsed = new SemanticVersion("17","05",123,"release") }
+        };
+        
+        [Test]
+        public void CanParse([ValueSource(nameof(Cases))] Case testCase)
+        {
+            var parsed = SemanticVersion.FromString(testCase.Raw);
+            Assert.That(parsed.Major, Is.EqualTo(testCase.Parsed.Major));
+            Assert.That(parsed.Minor, Is.EqualTo(testCase.Parsed.Minor));
+            Assert.That(parsed.Build, Is.EqualTo(testCase.Parsed.Build));
+            Assert.That(parsed.SemanticTag, Is.EqualTo(testCase.Parsed.SemanticTag));
+        }
+      
+        [Test]
+        public void CanFormat([ValueSource(nameof(Cases))] Case testCase)
+        {
+            Assert.That(testCase.Parsed.ToString(), Is.EqualTo(testCase.Raw));
+        }
+    }
+}
---
+++ Bluewire.Conventions/SemanticVersion.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Bluewire.Conventions
+{
+    // major.minor.build-semtag
+    public class SemanticVersion
+    {
+        public readonly static string[] KnownSemanticTags = new string[4] { "beta", "rc", "release", "canary" };
+
+        public bool IsComplete
+        {
+            get
+            {
+                return !string.IsNullOrWhiteSpace(SemanticTag) && KnownSemanticTags.Contains(SemanticTag);
+            }
+        }
+
+        public SemanticVersion(string major, string minor, int build, string semanticTag)
+        {
+            Major = major;
+            Minor = minor;
+            Build = build;
+            SemanticTag = semanticTag;
+        }
+
+        public SemanticVersion(string versionNumber, int buildNumber, BranchType branchType)
+        {
+            var majorMinor = versionNumber.Split('.');
+            Major = majorMinor[0];
+            Minor = majorMinor[1];
+            Build = buildNumber;
+            SemanticTag = branchType.SemanticTag;
+        }
+        
+        public static SemanticVersion FromString(string semVer)
+        {
+            var m = Patterns.SemanticVersionStructure.Match(semVer);
+            if (!m.Success)
+            {
+                throw new ArgumentException($"Unable to parse semantic version: {semVer}");
+            }
+            
+            return new SemanticVersion(m.Groups["major"].Value, m.Groups["minor"].Value, int.Parse(m.Groups["build"].Value), m.Groups["semtag"]?.Value);
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.AppendFormat("{0}.{1}.{2}", Major, Minor, Build);
+            if (!string.IsNullOrEmpty(SemanticTag)) sb.AppendFormat("-{0}", SemanticTag);
+            return sb.ToString();
+        }
+
+        public string Major { get; }
+        public string Minor { get; }
+        public int Build { get; }
+        public string SemanticTag { get; }
+
+        public static SemanticVersion FindEarliestSemanticTag(SemanticVersion[] originalVersions)
+        {
+            if (originalVersions.Length <= 0) throw new ArgumentException("No versions supplied.");
+
+            var betaVersion = originalVersions.Where(v => v.SemanticTag == "beta").SingleOrDefault();
+            if (betaVersion != null) return betaVersion;
+
+            var rcVersion = originalVersions.Where(v => v.SemanticTag == "rc").SingleOrDefault();
+            if (rcVersion != null) return rcVersion;
+
+            var releaseVersion = originalVersions.Where(v => v.SemanticTag == "release").SingleOrDefault();
+            if (releaseVersion != null) return releaseVersion;
+
+            throw new ArgumentException("No versions with a valid semantic tag supplied.");
+        }
+    }
+}
