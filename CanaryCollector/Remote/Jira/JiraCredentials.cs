075e97c5d8a47ccdf41396dc241d4a58e2400d45
Alex Davidson <alex.davidson@bluewire-technologies.com>
2020-01-31 21:09:45 +0000

    Update Jira interop assembly
    refs E-32710

---
+++ CanaryCollector/Remote/Jira/JiraCredentials.cs
@@ -0,0 +1,14 @@
+﻿namespace CanaryCollector.Remote.Jira
+{
+    class JiraCredentials
+    {
+        public string UserName { get; }
+        public string Password { get; }
+
+        public JiraCredentials(string userName, string password)
+        {
+            UserName = userName;
+            Password = password;
+        }
+    }
+}
