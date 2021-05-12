34afa0651cf907c240a01464ad6dea6b474b283a
Alex Davidson <alex.davidson@bluewire-technologies.com>
2021-05-12 12:40:15 +0100

    Teach Bluewire.Conventions about alpha, etc branches
    refs EP-24702
    
    Bluewire.Conventions: 2.0.1 -> 2.1.0

---
+++ Bluewire.Conventions.UnitTests/VersionSemanticsTests.cs
@@ -0,0 +1,68 @@
+﻿using NUnit.Framework;
+
+namespace Bluewire.Conventions.UnitTests
+{
+    [TestFixture]
+    public class VersionSemanticsTests
+    {
+        public struct IsAncestorCase
+        {
+            public string Reference { get; set; }
+            public string Subject { get; set; }
+            public int? LastSubjectMasterBuild { get; set; }
+            public bool Expected { get; set; }
+
+            public override string ToString()
+            {
+                var description = $"{Subject} ancestor of {Reference} -> {Expected}";
+                if (LastSubjectMasterBuild == null) return description;
+                return description + $" (Build {LastSubjectMasterBuild} is in master)";
+            }
+        }
+
+        public static IsAncestorCase[] IsAncestorCases =
+        {
+            // Release/candidate/etc branches of same major.minor has major.minor.0-beta as an ancestor.
+            new IsAncestorCase { Reference = "20.21.10-beta", Subject = "20.21.0-beta", Expected = true },
+            new IsAncestorCase { Reference = "20.21.10-rc", Subject = "20.21.0-beta", Expected = true },
+            new IsAncestorCase { Reference = "20.21.10-release", Subject = "20.21.0-beta", Expected = true },
+            // Same applies to alphas, etc.
+            new IsAncestorCase { Reference = "20.21.10-alpha.g35897262", Subject = "20.21.0-beta", Expected = true },
+            new IsAncestorCase { Reference = "20.21.10-canary", Subject = "20.21.0-beta", Expected = true },
+            // But alphas, etc do not have non-zero builds as ancestors.
+            new IsAncestorCase { Reference = "20.21.20-alpha.g35897262", Subject = "20.21.10-beta", Expected = false },
+            new IsAncestorCase { Reference = "20.21.20-canary", Subject = "20.21.10-beta", Expected = false },
+            // Alphas do have previous major.minor.0-betas as ancestors.
+            new IsAncestorCase { Reference = "20.21.20-alpha.g35897262", Subject = "20.20.0-beta", Expected = true },
+            new IsAncestorCase { Reference = "20.21.20-canary", Subject = "20.20.0-beta", Expected = true },
+            // But not non-zero builds of previous major.minors.
+            new IsAncestorCase { Reference = "20.21.20-alpha.g35897262", Subject = "20.20.10-beta", Expected = false },
+            new IsAncestorCase { Reference = "20.21.20-canary", Subject = "20.20.10-beta", Expected = false },
+            // Unless we know they're in the ancestry of subsequent major.minors.
+            new IsAncestorCase { Reference = "20.21.20-alpha.g35897262", Subject = "20.20.10-beta", LastSubjectMasterBuild = 100, Expected = true },
+            new IsAncestorCase { Reference = "20.21.20-canary", Subject = "20.20.10-beta", LastSubjectMasterBuild = 100, Expected = true },
+            new IsAncestorCase { Reference = "20.21.20-beta", Subject = "20.20.10-beta", LastSubjectMasterBuild = 100, Expected = true },
+            new IsAncestorCase { Reference = "20.21.20-rc", Subject = "20.20.10-beta", LastSubjectMasterBuild = 100, Expected = true },
+            new IsAncestorCase { Reference = "20.21.20-release", Subject = "20.20.10-beta", LastSubjectMasterBuild = 100, Expected = true },
+
+            // Cannot compare across different tags.
+            new IsAncestorCase { Reference = "20.21.20-rc", Subject = "20.21.10-beta", Expected = false },
+            new IsAncestorCase { Reference = "20.21.20-release", Subject = "20.21.10-rc", Expected = false },
+            new IsAncestorCase { Reference = "20.21.20-release", Subject = "20.21.10-beta", Expected = false },
+            new IsAncestorCase { Reference = "20.21.10-rc", Subject = "20.21.20-beta", Expected = false },
+            new IsAncestorCase { Reference = "20.21.10-release", Subject = "20.21.20-rc", Expected = false },
+            new IsAncestorCase { Reference = "20.21.10-release", Subject = "20.21.20-beta", Expected = false },
+            // Unless subject is in the ancestry of subsequent major.minors too.
+            new IsAncestorCase { Reference = "20.21.20-rc", Subject = "20.21.10-beta", LastSubjectMasterBuild = 15, Expected = true },
+            new IsAncestorCase { Reference = "20.21.20-release", Subject = "20.21.10-beta", LastSubjectMasterBuild = 15, Expected = true },
+        };
+
+        [Test]
+        public void IsAncestor([ValueSource(nameof(IsAncestorCases))] IsAncestorCase testCase)
+        {
+            var reference = SemanticVersion.FromString(testCase.Reference);
+            var subject = SemanticVersion.FromString(testCase.Subject);
+            Assert.That(new VersionSemantics().IsAncestor(reference, subject, testCase.LastSubjectMasterBuild), Is.EqualTo(testCase.Expected));
+        }
+    }
+}
---
+++ Bluewire.Conventions/VersionSemantics.cs
@@ -0,0 +1,114 @@
+﻿namespace Bluewire.Conventions
+{
+    public class VersionSemantics
+    {
+        public SemanticVersion GetStartOfMajorMinor(SemanticVersion semVer) => new SemanticVersion(semVer.Major, semVer.Major, 0, "beta");
+
+        /// <summary>
+        /// Returns true if 'subject' is likely to be in the ancestry chain of 'reference'.
+        /// </summary>
+        /// <remarks>
+        /// This cannot be 100% accurate since it does not have knowledge of the actual topology.
+        /// It implements heuristics based on our workflows.
+        /// If 'lastSubjectMasterBuild' is not null, it specifies the build number of the last beta
+        /// build of subject's major.minor on the master branch.
+        /// </remarks>
+        public bool IsAncestor(SemanticVersion reference, SemanticVersion subject, int? lastSubjectMasterBuild = null)
+        {
+            // If the subject is not canonical, we cannot determine whether it is an ancestor.
+            if (!IsCanonicalVersion(subject)) return false;
+
+            var referenceMajor = int.Parse(reference.Major);
+            var referenceMinor = int.Parse(reference.Minor);
+
+            var subjectMajor = int.Parse(subject.Major);
+            var subjectMinor = int.Parse(subject.Minor);
+
+            if (referenceMajor < subjectMajor) return false;
+
+            if (referenceMajor == subjectMajor)
+            {
+                if (referenceMinor < subjectMinor) return false;
+                if (referenceMinor == subjectMinor)
+                {
+                    // Same major.minor. Can compare build numbers.
+
+                    // If reference's build number is before subject's, subject cannot possibly be in reference's ancestory.
+                    if (reference.Build < subject.Build) return false;
+
+                    // Subject's build number is before reference's.
+
+                    // If tags are the same, they're in the same first-parent ancestory chain.
+                    if (reference.SemanticTag == subject.SemanticTag) return true;
+
+                    // If subject's build number is before the next major.minor was created, it is in the ancestory of all release and rc branches of that major.minor.
+                    if (subject.Build <= lastSubjectMasterBuild)
+                    {
+                        switch (reference.SemanticTag)
+                        {
+                            case "rc":
+                            case "release":
+                                return true;
+                        }
+                    }
+                    // If subject is the initial build of this version, it must be in the ancestry even though the reference tag is not canonical.
+                    if (subject.Build == 0) return true;
+
+                    // Cannot otherwise sanely compare build numbers from different tags unless we have more topology information.
+                    return false;
+                }
+            }
+
+            // Reference's major.minor is newer than subject's.
+            // If subject is the initial build of that version, it is most definitely in the ancestory of reference's major.minor.0.
+            if (subject.Build == 0) return true;
+            // If subject's build number is before the next major.minor was created, it is in the ancestory of reference's major.minor.0.
+            if (lastSubjectMasterBuild != null && subject.Build <= lastSubjectMasterBuild) return true;
+            return false;
+        }
+
+        /// <summary>
+        /// Returns true if the specified version is 'canonical', ie. its tag uniquely identifies an ancestry chain.
+        /// Release, candidate and beta/trunk branches have canonical history, while alpha or canary branches do not.
+        /// </summary>
+        public bool IsCanonicalVersion(SemanticVersion semVer)
+        {
+            switch (semVer.SemanticTag)
+            {
+                case "beta":
+                case "rc":
+                case "release":
+                    return true;
+
+                default:
+                    return false;
+            }
+        }
+
+        /// <summary>
+        /// Get the minimum number of merge commits to integrate 'subjectTag' into 'referenceTag'.
+        /// </summary>
+        private int? GetRelativeWeakness(string referenceTag, string subjectTag)
+        {
+            if (referenceTag == "beta")
+            {
+                if (subjectTag == "beta") return 0;
+                return null;
+            }
+            if (referenceTag == "rc")
+            {
+                if (subjectTag == "beta") return 1;
+                if (subjectTag == "rc") return 0;
+                return null;
+            }
+            if (referenceTag == "release")
+            {
+                if (subjectTag == "beta") return 2;
+                if (subjectTag == "rc") return 1;
+                if (subjectTag == "release") return 0;
+                return null;
+            }
+            return null;
+        }
+    }
+}
