8cc00b3c163a17652d9aacfbec9fe2d6d6c91b1c
Tom Williamson <thomas@epro.com>
2021-11-04 11:38:13 +0000

    Enable Jira package user privacy mode
    ref: EP-42945

--- CanaryCollector/Remote/Jira/JiraTicketProviderFactory.cs
+++ CanaryCollector/Remote/Jira/JiraTicketProviderFactory.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Atlassian.Jira;
 using CanaryCollector.Collectors;
 
 namespace CanaryCollector.Remote.Jira
@@ -23,7 +24,14 @@ namespace CanaryCollector.Remote.Jira
 
         public ITicketProvider Create(string dependentParameter)
         {
-            var jiraConnection = Atlassian.Jira.Jira.CreateRestClient(jiraUri.ToString(), credentials.UserName, credentials.Password);
+            var settings = new JiraRestClientSettings();
+
+            // This is required due to GDPR changes specific to the Cloud version https://bitbucket.org/farmas/atlassian.net-sdk/issues/509/jirauser-api-v2-updated-for-gdpr-removing.
+            // without this, the package can't understand custom user fields that Jira Cloud refuses to return as the expected user object.
+            // This means we will be returned account IDs rather than any user information. We don't require user information in this tool.
+            settings.EnableUserPrivacyMode = true;
+
+            var jiraConnection = Atlassian.Jira.Jira.CreateRestClient(jiraUri.ToString(), credentials.UserName, credentials.Password, settings);
             return new JiraTicketProvider(jiraConnection);
         }
     }
