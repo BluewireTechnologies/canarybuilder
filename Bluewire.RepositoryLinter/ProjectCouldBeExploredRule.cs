62d5370c07716714b115a09e354d9357f6d0ff2f
Alex Davidson <alda@epro.com>
2024-06-27 15:05:10 +0100

    Report failures to load projects
    Fix handling of 'Update' PackageReferences.
    
    refs EP-47999

---
+++ Bluewire.RepositoryLinter/ProjectCouldBeExploredRule.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+using Bluewire.Common.GitWrapper.Model;
+
+namespace Bluewire.RepositoryLinter;
+
+public class ProjectCouldBeExploredRule
+{
+    private readonly SubjectRepository subject;
+
+    public ProjectCouldBeExploredRule(SubjectRepository subject)
+    {
+        this.subject = subject;
+    }
+
+    public IEnumerable<Failure> GetFailures(Ref branch, ImmutableArray<ProjectFile> projects)
+    {
+        if (!subject.GetBranchRules(branch).ReportLoadFailures) yield break;
+
+        foreach (var project in projects)
+        {
+            if (project.Exception != null)
+            {
+                yield return new Failure
+                {
+                    Subject = subject,
+                    Message = $"Project could not be loaded: {project.Exception.Message}",
+                    Branch = branch,
+                    ProjectFile = project,
+                };
+            }
+        }
+    }
+}
