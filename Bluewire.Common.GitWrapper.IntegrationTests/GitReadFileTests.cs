709313932854e243c173d2362f14bb8ec2a1bd3f
Alex Davidson <alda@epro.com>
2024-06-27 13:59:29 +0100

    Add support for reading files directly out of the repository
    refs EP-47999

---
+++ Bluewire.Common.GitWrapper.IntegrationTests/GitReadFileTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Bluewire.Common.GitWrapper.Model;
+using NUnit.Framework;
+
+namespace Bluewire.Common.GitWrapper.IntegrationTests
+{
+    [TestFixture]
+    public class GitReadFileTests
+    {
+        private GitSession session;
+        private GitWorkingCopy workingCopy;
+
+        private static Ref MasterBranch => new Ref("master");
+
+        [SetUp]
+        public async Task SetUp()
+        {
+            session = await Default.GitSession();
+            workingCopy = await session.Init(Default.TemporaryDirectory, "repository");
+            await session.Commit(workingCopy, "Initial commit", CommitOptions.AllowEmptyCommit);
+        }
+
+        [Test]
+        public async Task CanReadFile()
+        {
+            var original = new byte[4096];
+            new Random().NextBytes(original);
+            File.WriteAllBytes(workingCopy.Path("testfile"), original);
+            await session.AddFile(workingCopy, "testfile");
+            await session.Commit(workingCopy, "test");
+
+            await session.Checkout(workingCopy, Ref.Head.Parent());
+
+            using (var stream = new MemoryStream())
+            {
+                await session.ReadFile(workingCopy, MasterBranch, "testfile", stream);
+                stream.Position = 0;
+                var roundtripped = stream.ToArray();
+                Assert.That(roundtripped, Is.EqualTo(original));
+            }
+        }
+
+        [Test]
+        public async Task CanCheckFileExists()
+        {
+            var original = new byte[4096];
+            new Random().NextBytes(original);
+            File.WriteAllBytes(workingCopy.Path("testfile"), original);
+            await session.AddFile(workingCopy, "testfile");
+            await session.Commit(workingCopy, "test");
+
+            await session.Checkout(workingCopy, Ref.Head.Parent());
+
+            Assert.That(await session.FileExists(workingCopy, MasterBranch, "testfile"), Is.True);
+            Assert.That(await session.FileExists(workingCopy, MasterBranch, "doesnotexist"), Is.False);
+        }
+    }
+}
